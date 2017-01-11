6952885e7ba296723a9c0210361d1e219d6dbdd5
kbeloborodko <kbeloborodko@gmail.com>
2017-01-11 17:52:44 +0200

    Small changes to make the right usage of the variable declarations keywords has been made

--- block-scope/block-scope-fixed.js
+++ block-scope/block-scope-fixed.js
@@ -1,10 +1,13 @@
-var x = 2, fns = [];
+var x = 2;
+var fns = [];
 
 (function(){
-    var x = 5;
+    let x = 5;
 
-    for (var i=0; i<x; i++) {
-        // ..
+    for (let i=0; i<x; i++) {
+        fns[i] = function () {
+            return i;
+        };
     }
 })();
 
