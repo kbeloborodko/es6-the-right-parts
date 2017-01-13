b87992f4e8b44f0a869c97bb12a440e754f35f4d
kbeloborodko <kbeloborodko@gmail.com>
2017-01-13 18:28:19 +0200

    A more sophisticated and correct solution

--- symbols-iterators-generators/symbols-iterators-generators-fixed.js
+++ symbols-iterators-generators/symbols-iterators-generators-fixed.js
@@ -1,25 +1,25 @@
-var numbers1 = {
-  *[Symbol.iterator]() {
-    for (var i = 0; i <= 100; i++) {
-      yield i;
-    }
-  }
-};
-
-var numbers2 = {
-  *[Symbol.iterator]() {
-    for (var i = 6; i <= 30; i = i + 4) {
+var numbers = {
+  *[Symbol.iterator]({
+    start = 0,
+    step = 1,
+    end = 100
+  } = {}) {
+    for (let i = start; i <= end; i = i + step) {
       yield i;
     }
   }
 };
 
 // should print 0..100 by 1s
-for (let num of numbers1) {
+for (let num of numbers) {
   console.log(num);
 }
 
 // should print 6..30 by 4s
-for (let num of numbers2) {
+for (let num of numbers[Symbol.iterator]({
+  start: 6,
+  step: 4,
+  end: 30
+})) {
   console.log(num);
 }
