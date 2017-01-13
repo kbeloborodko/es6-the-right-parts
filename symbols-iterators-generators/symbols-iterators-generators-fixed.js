8833c764fa4a47083e3b46a8192e6d5fa0b9fb9b
kbeloborodko <kbeloborodko@gmail.com>
2017-01-13 18:19:03 +0200

    Generator challenge first variant solution

--- symbols-iterators-generators/symbols-iterators-generators-fixed.js
+++ symbols-iterators-generators/symbols-iterators-generators-fixed.js
@@ -1,13 +1,25 @@
-var numbers = {
-  // ..
+var numbers1 = {
+  *[Symbol.iterator]() {
+    for (var i = 0; i <= 100; i++) {
+      yield i;
+    }
+  }
+};
+
+var numbers2 = {
+  *[Symbol.iterator]() {
+    for (var i = 6; i <= 30; i = i + 4) {
+      yield i;
+    }
+  }
 };
 
 // should print 0..100 by 1s
-for (let num of numbers) {
+for (let num of numbers1) {
   console.log(num);
 }
 
 // should print 6..30 by 4s
-for (let num of /*..*/) {
+for (let num of numbers2) {
   console.log(num);
 }
