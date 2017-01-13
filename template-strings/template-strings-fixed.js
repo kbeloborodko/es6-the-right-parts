a950de60675c936572e2a00883ada0dde2317662
kbeloborodko <kbeloborodko@gmail.com>
2017-01-13 11:55:43 +0200

    Solution has been added

--- template-strings/template-strings-fixed.js
+++ template-strings/template-strings-fixed.js
@@ -1,10 +1,22 @@
-function upper(strings,...values) {}
+function upper(strings,...values) {
+	var str = "";
+
+	for (var i = 0; i < strings.length; i++) {
+		str+= strings[i];
+
+		if (i !== strings.length - 1) {
+			str+= values[i].toUpperCase();
+		}
+	}
+
+	return str;
+}
 
 var name = "kyle",
   twitter = "getify",
   classname = "es6 workshop";
 
 console.log(
-  `Hello ____ (@____), welcome to the ____!` ===
+  upper`Hello ${name} (@${twitter}), welcome to the ${classname}!` ===
   "Hello KYLE (@GETIFY), welcome to the ES6 WORKSHOP!"
 );
